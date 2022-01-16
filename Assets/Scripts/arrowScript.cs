544e508016d5115c3e60efd76e37411f14330bf0
Saahil-Shelke <saahil.shelke@gmail.com>
2022-01-16 19:25:12 +0530

    bg object

---
+++ Assets/Scripts/arrowScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowScript : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    bool hasHit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.tag = "Weapon";
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if(hasHit == false)
+        {
+            trackMovement();
+        }
+
+    }
+
+    void trackMovement()
+    {
+        Vector2 direction = rb.velocity;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); 
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        hasHit = true;
+        rb.velocity = Vector2.zero;
+
+        Destroy(gameObject, 1f);
+    }
+
+}
---
+++ Assets/Scripts/image clearer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class imageclearer : MonoBehaviour
+{
+    void Awake()
+{
+    SpriteRenderer sr = GetComponent<SpriteRenderer>();
+    if (sr == null) return;
+
+    transform.localScale = new Vector3(1, 1, 1);
+
+    float width = sr.sprite.bounds.size.x;
+    float height = sr.sprite.bounds.size.y;
+
+    transform.localScale = new Vector2(Screen.width / width, Screen.height / height);
+}
+}
