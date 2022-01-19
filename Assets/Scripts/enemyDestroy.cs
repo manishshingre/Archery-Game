232c6197b1a8bddd783194973827510c0dcb7140
Bipin_Giri <bipingiri0085@gmail.com>
2022-01-20 02:29:29 +0530

    blood splash effect

--- Assets/Scripts/enemyDestroy.cs
+++ Assets/Scripts/enemyDestroy.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class enemyDestroy : MonoBehaviour
 {
+
+    // public GameObject blood;
+    UnityEngine.Object blood;
     private void OnCollisionEnter2D(Collision2D other){
-        if(other.gameObject.CompareTag("Weapon")){
-            Destroy(gameObject);
+        if(other.gameObject.tag.Equals("Weapon")){
+            // Instantiate(blood, transform.position, Quaternion.identity);
+            // Destroy(other.gameObject);
+            // Destroy(gameObject);
+            BloodSplash();
         }
     }
+
+    private void BloodSplash()
+    {
+        GameObject splash = (GameObject)Instantiate(blood);
+        splash.transform.position = transform.position;
+        Destroy(gameObject);
+    }
+
 }
